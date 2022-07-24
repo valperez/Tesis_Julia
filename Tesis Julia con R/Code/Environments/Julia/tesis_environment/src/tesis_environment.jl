f79bbc24c06d69752332f9ea0bad95c5dc0e5821
valperez <valperez1214@gmail.com>
2022-07-23 18:17:03 -0600

    environemnts cargados

---
+++ Tesis Julia con R/Code/Environments/Julia/tesis_environment/src/tesis_environment.jl
@@ -0,0 +1,5 @@
+module tesis_environment
+
+greet() = print("Hello World!")
+
+end # module
---
+++ Tesis Julia con R/Code/Environments/R/R_Environment.R
@@ -0,0 +1,30 @@
+print(sessionInfo())
+
+# R version 4.2.1 (2022-06-23 ucrt)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19044)
+# 
+# Matrix products: default
+# 
+# locale:
+#   [1] LC_COLLATE=Spanish_Mexico.utf8  LC_CTYPE=Spanish_Mexico.utf8    LC_MONETARY=Spanish_Mexico.utf8
+# [4] LC_NUMERIC=C                    LC_TIME=Spanish_Mexico.utf8    
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] reticulate_1.25  JuliaCall_0.17.4 BsMD_2020.4.30   BsMD2_0.0.0.9000 forcats_0.5.1    purrr_0.3.4     
+# [7] readr_2.1.2      tidyr_1.2.0      tibble_3.1.7     ggplot2_3.3.6    tidyverse_1.3.1  stringr_1.4.0   
+# [13] dplyr_1.0.9      pracma_2.3.8     knitr_1.39       polynom_1.4-1   
+# 
+# loaded via a namespace (and not attached):
+#   [1] Rcpp_1.0.8.3     lattice_0.20-45  lubridate_1.8.0  png_0.1-7        assertthat_0.2.1 digest_0.6.29   
+# [7] utf8_1.2.2       R6_2.5.1         cellranger_1.1.0 backports_1.4.1  reprex_2.0.1     evaluate_0.15   
+# [13] httr_1.4.3       pillar_1.7.0     rlang_1.0.3      readxl_1.4.0     rstudioapi_0.13  Matrix_1.4-1    
+# [19] rmarkdown_2.14   munsell_0.5.0    broom_1.0.0      compiler_4.2.1   modelr_0.1.8     xfun_0.31       
+# [25] pkgconfig_2.0.3  htmltools_0.5.2  tidyselect_1.1.2 fansi_1.0.3      crayon_1.5.1     tzdb_0.3.0      
+# [31] dbplyr_2.2.1     withr_2.5.0      grid_4.2.1       jsonlite_1.8.0   gtable_0.3.0     lifecycle_1.0.1 
+# [37] DBI_1.1.3        magrittr_2.0.3   scales_1.2.0     cli_3.3.0        stringi_1.7.6    fs_1.5.2        
+# [43] xml2_1.3.3       ellipsis_0.3.2   generics_0.1.2   vctrs_0.4.1      tools_4.2.1      glue_1.6.2      
+# [49] hms_1.1.1        fastmap_1.1.0    yaml_2.3.5       colorspace_2.0-3 rvest_1.0.2      haven_2.5.0 
